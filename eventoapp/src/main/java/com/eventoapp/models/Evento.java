9594786419b3acd17ff49e1d291fefbd83e76073
Kiko <kikollovet@gmail.com>
2021-01-23 17:22:02 -0300

    aula 9

--- eventoapp/src/main/java/com/eventoapp/models/Evento.java
+++ eventoapp/src/main/java/com/eventoapp/models/Evento.java
@@ -1,11 +1,13 @@
 package com.eventoapp.models;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Evento implements Serializable{
@@ -20,6 +22,9 @@ public class Evento implements Serializable{
 	private String local;
 	private String data;
 	private String horario;
+	
+	@OneToMany
+	private List<Convidado> convidados;
 	
 	public long getCodigo() {
 		return codigo;
@@ -60,5 +65,15 @@ public class Evento implements Serializable{
 	public void setHorario(String horario) {
 		this.horario = horario;
 	}
+
+	public List<Convidado> getConvidados() {
+		return convidados;
+	}
+
+	public void setConvidado(List<Convidado> convidados) {
+		this.convidados = convidados;
+	}
+	
+	
 	
 }
